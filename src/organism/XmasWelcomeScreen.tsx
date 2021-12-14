b6be8129337936bc2d553f7dfc00495257d253a3
zimiin <kahin6105@gmail.com>
2021-12-14 23:59:17 +0900

    Add x-mas tree

--- src/organism/XmasWelcomeScreen.tsx
+++ src/organism/XmasWelcomeScreen.tsx
@@ -1,19 +1,20 @@
 import LightBulb from 'atom/LightBulb'
 import React from 'react'
 import styled from 'styled-components'
+import xmasTree from 'images/xmas_tree.png'
 
 const Container = styled.div`
   background: #202124;
   height: 100%;
   position: relative;
   padding-top: 100px;
-  padding-left: 50px;
+  // padding-left: 50px;
 `
 
 function XmasWelcomeScreen() {
   return (
     <Container>
-      <LightBulb />
+      {/* <LightBulb />
       <LightBulb 
         color='#538FE4'
       />
@@ -22,7 +23,14 @@ function XmasWelcomeScreen() {
       />
       <LightBulb
         color='#45CC7B'
-      />
+      /> */}
+
+      <img src={xmasTree} style={{
+        // margin: 'auto',
+        marginLeft: 'auto',
+        marginRight: 'auto',
+        display: 'block',
+      }}/>
     </Container>
   )
 }
