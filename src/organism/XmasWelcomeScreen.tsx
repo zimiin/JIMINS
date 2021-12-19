394323d6f14bf4c7e25ce4ec9009e028d2d0263d
zimiin <kahin6105@gmail.com>
2021-12-19 16:46:10 +0900

    🎨 Show light bulbs on random position

--- src/organism/XmasWelcomeScreen.tsx
+++ src/organism/XmasWelcomeScreen.tsx
@@ -1,8 +1,8 @@
 import LightBulb from 'atom/LightBulb'
-import React from 'react'
+import React, { useMemo } from 'react'
 import styled from 'styled-components'
 import xmasTree from 'images/xmas_tree.png'
-import { verticalScale } from 'utils/size'
+import { BROWSER_HEIGHT, BROWSER_WIDTH, verticalScale } from 'utils/size'
 
 const Container = styled.div`
   background: #202124;
@@ -14,19 +14,56 @@ const CenterTree = styled.div`
   padding-top: ${verticalScale(258)}px;
 `
 
+type LightBulbBoxType = {
+  top?: number;
+  left?: number;
+}
+
+const LightBulbBox = styled.div<LightBulbBoxType>`
+  position: absolute;
+  top: ${props => props.top ? props.top + 'px' : '100px'};
+  left: ${props => props.left ? props.left + 'px' : '200px'};
+`
+
+const lightBulbColors = ['#538FE4', '#FF7A76', '#45CC7B', '#FFDD7B']
+
+function getRandomInt(max: number) {
+  return Math.floor(Math.random() * max);
+}
+
+function getRandomArbitrary(min: number, max: number) {
+  return Math.random() * (max - min) + min;
+}
+
 function XmasWelcomeScreen() {
+  const lightBulbs = useMemo(() => {
+    const result = []
+    const rectHeight = 200
+    const rectWidth = 250
+    const noCol = Math.floor(BROWSER_WIDTH / rectWidth)
+    const noRow = Math.floor(BROWSER_HEIGHT / rectHeight)
+
+    for (let i = 0; i < noRow; i++) {
+      for (let j = 0; j < noCol; j++) {
+        result.push(
+          <LightBulbBox
+            top={rectHeight * i + getRandomArbitrary(15, rectHeight - 14) + (BROWSER_HEIGHT % rectHeight) / 2}
+            left={rectWidth * j + getRandomArbitrary(15, rectWidth - 14) + (BROWSER_WIDTH % rectWidth) / 2}
+          >
+            <LightBulb
+              color={lightBulbColors[getRandomInt(4)]}
+            />
+          </LightBulbBox>
+        )
+      }
+    }
+
+    return result
+  }, [window.innerHeight, window.innerWidth])
+
   return (
     <Container>
-      {/* <LightBulb />
-      <LightBulb 
-        color='#538FE4'
-      />
-      <LightBulb
-        color='#FF7A76'
-      />
-      <LightBulb
-        color='#45CC7B'
-      /> */}
+      {lightBulbs}
 
       <CenterTree>
         <img src={xmasTree} style={{
@@ -34,6 +71,8 @@ function XmasWelcomeScreen() {
           marginLeft: 'auto',
           marginRight: 'auto',
           display: 'block',
+          position: 'relative',
+          zIndex: '100'
         }}/>
       </CenterTree>
     </Container>
--- src/utils/size.ts
+++ src/utils/size.ts
@@ -1,5 +1,6 @@
 export const DESIGN_HEIGHT = 1024
 export const BROWSER_HEIGHT = window.innerHeight
+export const BROWSER_WIDTH = window.innerWidth
 
 export function verticalScale(height: number) {
   return (height / DESIGN_HEIGHT) * BROWSER_HEIGHT
