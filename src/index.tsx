69fcb89f545a76840b39ff9d873f2c753b9a53fc
zimiin <kahin6105@gmail.com>
2022-05-22 22:10:57 +0900

    feat: Wordle 라우트 추가

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import './index.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import IndexPage from 'page/IndexPage'
 import NotFoundPage from 'page/NotFoundPage'
+import Wordle from 'wordle/Wordle'
 
 ReactDOM.render(
   <BrowserRouter>
@@ -11,6 +12,7 @@ ReactDOM.render(
       <Route path='/' element={<IndexPage />}>
         <Route path='*' element={<NotFoundPage />} />
       </Route>
+      <Route path='/wordle' element={<Wordle />} />
     </Routes>
   </BrowserRouter>,
   document.getElementById('root')
---
+++ src/wordle/Wordle.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+const Wordle = () => {
+  return (
+    <div>
+      <h1 className='font-bold underline'>Hello wordle</h1>
+    </div>
+  )
+}
+
+export default Wordle
