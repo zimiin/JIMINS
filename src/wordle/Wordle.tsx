3b3b4a0378373fc6a4a72398a8177682e49a0ea6
zimiin <kahin6105@gmail.com>
2022-05-24 00:17:22 +0900

    feat: 게임 틀 완성

--- src/wordle/Wordle.tsx
+++ src/wordle/Wordle.tsx
@@ -1,9 +1,54 @@
-import React from "react"
+import React from 'react'
+import './Wordle.css'
 
 const Wordle = () => {
   return (
-    <div>
+    <div className='wordle-container'>
       <h1 className='font-bold underline'>Hello wordle</h1>
+      <div>
+        <div className='block-row'>
+          <div className='block' />
+          <div className='block' />
+          <div className='block' />
+          <div className='block' />
+          <div className='block' />
+        </div>
+        <div className='block-row'>
+          <div className='block' />
+          <div className='block' />
+          <div className='block' />
+          <div className='block' />
+          <div className='block' />
+        </div>
+        <div className='block-row'>
+          <div className='block' />
+          <div className='block' />
+          <div className='block' />
+          <div className='block' />
+          <div className='block' />
+        </div>
+        <div className='block-row'>
+          <div className='block' />
+          <div className='block' />
+          <div className='block' />
+          <div className='block' />
+          <div className='block' />
+        </div>
+        <div className='block-row'>
+          <div className='block' />
+          <div className='block' />
+          <div className='block' />
+          <div className='block' />
+          <div className='block' />
+        </div>
+        <div className='block-row'>
+          <div className='block' />
+          <div className='block' />
+          <div className='block' />
+          <div className='block' />
+          <div className='block' />
+        </div>
+      </div>
     </div>
   )
 }
