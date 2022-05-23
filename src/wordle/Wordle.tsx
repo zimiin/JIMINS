0c19c3d2c2968cce92ec4019a148b54f30adfb67
zimiin <kahin6105@gmail.com>
2022-05-24 00:30:08 +0900

    feat: words state 사용

--- src/wordle/Wordle.tsx
+++ src/wordle/Wordle.tsx
@@ -1,53 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Wordle.css'
 
+const MAX_ATTEMPT = 6
+const MAX_LETTER = 5
+
 const Wordle = () => {
+  const [words, setWords] = useState<string[][]>(Array(MAX_ATTEMPT).fill(Array(MAX_LETTER).fill('')))
+
   return (
     <div className='wordle-container'>
       <h1 className='font-bold underline'>Hello wordle</h1>
       <div>
-        <div className='block-row'>
-          <div className='block' />
-          <div className='block' />
-          <div className='block' />
-          <div className='block' />
-          <div className='block' />
-        </div>
-        <div className='block-row'>
-          <div className='block' />
-          <div className='block' />
-          <div className='block' />
-          <div className='block' />
-          <div className='block' />
-        </div>
-        <div className='block-row'>
-          <div className='block' />
-          <div className='block' />
-          <div className='block' />
-          <div className='block' />
-          <div className='block' />
-        </div>
-        <div className='block-row'>
-          <div className='block' />
-          <div className='block' />
-          <div className='block' />
-          <div className='block' />
-          <div className='block' />
-        </div>
-        <div className='block-row'>
-          <div className='block' />
-          <div className='block' />
-          <div className='block' />
-          <div className='block' />
-          <div className='block' />
-        </div>
-        <div className='block-row'>
-          <div className='block' />
-          <div className='block' />
-          <div className='block' />
-          <div className='block' />
-          <div className='block' />
-        </div>
+        {words.map((word) => (
+          <div className='block-row'>
+            {word.map((letter) => (
+              <div className='block'>{letter}</div>
+            ))}
+          </div>
+        ))}
       </div>
     </div>
   )
