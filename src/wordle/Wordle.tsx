5c2e482ea7934db7f374e7c5ae180712b3f7e89e
신지민 <kahin6105@gmail.com>
2022-05-30 00:19:48 +0900

    keyPress 이벤트 핸들러 추가

--- src/wordle/Wordle.tsx
+++ src/wordle/Wordle.tsx
@@ -7,14 +7,19 @@ const MAX_LETTER = 5
 const Wordle = () => {
   const [words, setWords] = useState<string[][]>(Array(MAX_ATTEMPT).fill(Array(MAX_LETTER).fill('')))
 
+  const handleKeyPress: React.KeyboardEventHandler<HTMLDivElement> = (e) => {
+    console.log('hihihi')
+    console.log(e.key)
+  }
+
   return (
-    <div className='wordle-container'>
+    <div className='wordle-container' onKeyPress={handleKeyPress}>
       <h1 className='font-bold underline'>Hello wordle</h1>
       <div>
-        {words.map((word) => (
-          <div className='block-row'>
-            {word.map((letter) => (
-              <div className='block'>{letter}</div>
+        {words.map((word, wordIdx) => (
+          <div className='block-row' key={`row-${wordIdx}`}>
+            {word.map((letter, letterIdx) => (
+              <div className='block' key={`row-${wordIdx}-letter-${letterIdx}`}>{letter}</div>
             ))}
           </div>
         ))}
