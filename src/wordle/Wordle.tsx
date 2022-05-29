48628783d88f3a6f563479f0e0e23f5541b5976e
신지민 <kahin6105@gmail.com>
2022-05-30 00:25:26 +0900

    tabIndex 속성 추가, 이벤트 변경

--- src/wordle/Wordle.tsx
+++ src/wordle/Wordle.tsx
@@ -7,13 +7,13 @@ const MAX_LETTER = 5
 const Wordle = () => {
   const [words, setWords] = useState<string[][]>(Array(MAX_ATTEMPT).fill(Array(MAX_LETTER).fill('')))
 
-  const handleKeyPress: React.KeyboardEventHandler<HTMLDivElement> = (e) => {
+  const handleKeyDown: React.KeyboardEventHandler<HTMLDivElement> = (e) => {
     console.log('hihihi')
     console.log(e.key)
   }
 
   return (
-    <div className='wordle-container' onKeyPress={handleKeyPress}>
+    <div className='wordle-container' onKeyDown={handleKeyDown} tabIndex={0}>
       <h1 className='font-bold underline'>Hello wordle</h1>
       <div>
         {words.map((word, wordIdx) => (
