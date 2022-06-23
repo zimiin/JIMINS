5fbe4ddb663f510023e26b132061efbc0091cc61
Jimin Shin <45932570+zimiin@users.noreply.github.com>
2022-06-23 14:37:58 +0900

    words state 자료구조 변경

--- src/wordle/Wordle.tsx
+++ src/wordle/Wordle.tsx
@@ -5,9 +5,11 @@ const MAX_ATTEMPT = 6
 const MAX_LETTER = 5
 
 const Wordle = () => {
-  const [words, setWords] = useState<string[][]>(Array(MAX_ATTEMPT).fill(Array(MAX_LETTER).fill('')))
+  const [words, setWords] = useState<string[]>(Array(MAX_ATTEMPT * MAX_LETTER).fill(''))
+  const [attempt, setAttempt] = useState(0)
 
   const handleKeyDown: React.KeyboardEventHandler<HTMLDivElement> = (e) => {
+    // attempt 처리
     console.log('hihihi')
     console.log(e.key)
   }
@@ -15,13 +17,9 @@ const Wordle = () => {
   return (
     <div className='wordle-container' onKeyDown={handleKeyDown} tabIndex={0}>
       <h1 className='font-bold underline'>Hello wordle</h1>
-      <div>
-        {words.map((word, wordIdx) => (
-          <div className='block-row' key={`row-${wordIdx}`}>
-            {word.map((letter, letterIdx) => (
-              <div className='block' key={`row-${wordIdx}-letter-${letterIdx}`}>{letter}</div>
-            ))}
-          </div>
+      <div className=''>
+        {words.map((letter, letterIdx) => (
+          <div className='block' key={`letter-${letterIdx}`}>{letter}</div>
         ))}
       </div>
     </div>
